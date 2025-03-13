9337e96af959fbb282719b8017207470333e183d
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-03-13 16:25:10 +0600

    i have added dogs section all data

--- script/app.js
+++ script/app.js
@@ -18,6 +18,48 @@ const displayCategoriesData = (categories) => {
         <button class="flex items-center btn border-2 p-2 md:p-6"><img class="w-5 md:w-8" src="${cat.category_icon}" alt="this is category image"> ${cat.category}</button>
         `
         allBtn.appendChild(newDiv)
-        console.log(cat.category);
+    });
+}
+
+
+const getSingleCategory = async () => {
+    const response = await fetch('https://openapi.programming-hero.com/api/peddy/category/dog')
+    const data = await response.json()
+    displaySingleCategory(data.data);
+}
+getSingleCategory()
+
+const displaySingleCategory = (singleData) => {
+    singleData.forEach(dat => {
+        console.log(dat.petId);
+        const oneData = document.getElementById('one-data')
+        const newDiv = document.createElement('div')
+        newDiv.innerHTML = `
+            <!-- card  -->
+                <div class="p-3 border-2 rounded-lg">
+                    <img class="rounded-md md:rounded-xl" src="${dat.image}" alt="this is pets img">
+                    <h3 class="text-lg md:text-xl font-bold mt-2 mb-1 md:mt-4">${dat.pet_name}</h3>
+                    <div class="space-y-1 border-b-2 pb-3">
+                        <p class="flex gap-2 items-center text-primary-color"><img src="./assets/birth.svg"
+                                alt="">Breed: ${dat.breed}</p>
+                        <p class="flex gap-2 items-center text-primary-color"><img src="./assets/breed.svg"
+                                alt="">Birth: ${dat.date_of_birth}</p>
+                        <p class="flex gap-2 items-center text-primary-color"><img src="./assets/gender.svg"
+                                alt="">Gender: ${dat.gender}</p>
+                        <p class="flex gap-2 items-center text-primary-color"><img src="./assets/price.svg"
+                                alt="">Price: ${dat.price}</p>
+
+                    </div>
+                    <!-- bottom  -->
+                    <div class="pt-2 flex items-center justify-between">
+                        <button class="btn border-2 px-3 rounded-lg"><img src="./assets/like.svg" alt=""></button>
+                        <button class="btn border-2 px-3 rounded-lg text-btn-bg">Adopt</button>
+                        <button class="btn border-2 px-3 rounded-lg text-btn-bg">Details</button>
+                    </div>
+
+                </div>
+        `
+        oneData.appendChild(newDiv)
+
     });
 }
